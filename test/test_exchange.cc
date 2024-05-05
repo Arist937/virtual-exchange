c71cfe9b3f9356b20a900c524a2ca4888be61986
Arist937 <Aristotle937@gmail.com>
2024-05-05 19:37:32 +0800

    fix exchange test

--- test/test_exchange.cc
+++ test/test_exchange.cc
@@ -6,10 +6,10 @@ int main() {
 
     test_exchange.add_order("APPL", 0, 1.5, 50, quote_t::ASK);
     test_exchange.add_order("APPL", 0, 1.4, 20, quote_t::ASK);
-    test_exchange.add_order("APPL", 1, 1.51, 60, quote_t::ASK);
-    test_exchange.add_order("APPL", 1, 1.5, 20, quote_t::ASK);
+    test_exchange.add_order("APPL", 1, 1.51, 60, quote_t::BID);
+    test_exchange.add_order("APPL", 1, 1.5, 20, quote_t::BID);
     test_exchange.add_order("APPL", 0, 0.7, 20, quote_t::ASK);
-    test_exchange.add_order("APPL", 1, 0.6, 100, quote_t::ASK);
+    test_exchange.add_order("APPL", 1, 0.6, 100, quote_t::BID);
     test_exchange.add_order("APPL", 0, 0.3, 100, quote_t::ASK);
 
     return 0;
